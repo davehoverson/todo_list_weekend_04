02b3fd2cfddcb04ec50ac9c950c1642634f75dac
tongtonk <davidhoverson@yahoo.com>
2015-10-11 14:08:32 -0500

    added animation to css and updated app.js with mongo framework

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 var express = require('express');
 var path = require('path');
+var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 
 var app = express();
 
+var mongoose = require('mongoose');
+
+var mongoURI = "mongodb://localhost:27017/todolists";
+var MongoDB = mongoose.connect(mongoURI).connection;
+
+MongoDB.on('error', function (err) {
+  console.log('mongodb connection error:', err);
+});
+
+MongoDB.once('open', function () {
+  console.log('mongodb connection open!');
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
